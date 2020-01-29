910dd416173d5f3b404e2f0abbd23b356eb31f5b
mwangiwambui <wambui54mwangi@gmail.com>
2020-01-29 12:16:04 +0300

    STK push

---
+++ MPESA/app/Http/Controllers/MpesaController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Safaricom\Mpesa\Mpesa;
+class MpesaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $mpesa = new Mpesa();
+        $BusinessShortCode = 174379;
+        $Password =
+        $stk = $mpesa->STKPushSimulation(env('BusinessShortCode'), env('Passkey'), env('TransactionType'), env('Amount'), $request->p_number, env('PartyB'),$request->p_number, env('CallBackUrl'), 'account', 'test','remark'
+        );
+        return $stk;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ MPESA/app/Mpesa.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Mpesa extends Model
+{
+    protected $fillable= ['name','description'];
+    public function Mpesa(){
+
+    }
+
+}
