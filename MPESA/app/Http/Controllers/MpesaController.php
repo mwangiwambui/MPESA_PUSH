42c40b7a95a83ead9826dda3ae8f8183f69c26d0
mwangiwambui <wambui54mwangi@gmail.com>
2020-03-31 09:41:45 +0300

    Fixed Security Vulnerability

--- MPESA/app/Http/Controllers/MpesaController.php
+++ MPESA/app/Http/Controllers/MpesaController.php
@@ -11,79 +11,22 @@ class MpesaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function store(Request $request)
     {
         $mpesa = new Mpesa();
         $BusinessShortCode = 174379;
         $Password =
-        $stk = $mpesa->STKPushSimulation(env('BusinessShortCode'), env('Passkey'), env('TransactionType'), env('Amount'), $request->p_number, env('PartyB'),$request->p_number, env('CallBackUrl'), 'account', 'test','remark'
+        $stk = $mpesa->STKPushSimulation(env('BusinessShortCode'), env('Passkey'), env('TransactionType'), $request->amount, $request->p_number, env('PartyB'),$request->p_number, env('CallBackUrl'), 'account', 'test','remark'
         );
-        return $stk;
+        return back(compact('stk'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
